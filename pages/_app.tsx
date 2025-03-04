0ae6e5dc76b744680b6a422806aeee0090ac4d1e
Fumiya Otokozawa <fumiya.otokozawa@comsize.com>
2025-03-04 22:03:38 +0900

    vercelのWebAnalyticsを反映

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,13 @@ import "../styles/global.css";
 import type { AppProps } from "next/app";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import '../styles/calendar.css';
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
