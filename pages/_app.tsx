3fc527ff22bbc53fb0dc76ba626edc0f535dfdee
Fumiya Otokozawa <fumiya.otokozawa@comsize.com>
2025-03-04 22:06:11 +0900

    vercelのSpeedInsightsを反映

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import type { AppProps } from "next/app";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import '../styles/calendar.css';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
