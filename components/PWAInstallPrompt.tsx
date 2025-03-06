3c5c56815115712dcd1763048921fc9b930bb667
Fumiya Otokozawa <fumiya.otokozawa@comsize.com>
2025-03-06 11:50:59 +0900

    微調整

--- components/PWAInstallPrompt.tsx
+++ components/PWAInstallPrompt.tsx
@@ -29,14 +29,14 @@ const PWAInstallPrompt = () => {
       <div className="container mx-auto relative">
         <button 
           onClick={() => setShowPrompt(false)}
-          className="absolute right-2 top-1/2 -translate-y-1/2"
+          className="absolute right-0 top-1/2 -translate-y-1/2"
           aria-label="閉じる"
         >
-          <CloseIcon />
+          <CloseIcon fontSize="small" />
         </button>
-        <p className="text-xs text-center pr-8">
+        <p className="text-xs text-center pr-4">
           SOPHAをホーム画面に追加すると、より快適に利用できます<br/>
-          ※ブラウザで利用するとレイアウトが崩れる可能性があります！
+          ※ブラウザで利用するとレイアウトが崩れる可能性があります
         </p>
       </div>
     </div>
